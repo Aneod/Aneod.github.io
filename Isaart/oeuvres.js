930797d62d62303c4940c67a4714af525af37b00
Blys Digital <135101184+Aneod@users.noreply.github.com>
2023-07-01 13:19:31 +0200

    Add files via upload

---
+++ Isaart/oeuvres.js
@@ -0,0 +1,308 @@
+export var listeOeuvres;
+export var listeCategories;
+
+
+var listeCategories = [
+    ["Nature","Poissons, chats et quelques autres surprises !"],
+    ["Building","Des batiments étranges."],
+    ["Chat","Ils sont partout."],
+    ["Paysage","Prendre toujours un moment pour en profiter."],
+    ["Abstrait","Des idées mises en couleurs."],
+    ["Poisson","Avec des gros yeux ici."],
+    ["Essais technique","Pour progresser dans la technique !"],
+    ["Chouette","houhouuu !"]
+];
+
+
+var listeOeuvres = [
+
+    // [
+    //     Nom du tableau,
+    //     Description,
+    //     Dimensions,
+    //     Date,
+    //     Catégories,
+    //     L'image,
+    //     VRAI si dans la sélection, FAUX sinon
+    // ],
+
+    [
+        "Ponam",
+        "Une nouvelle technique et un clin d'oeil à la talentueuse illustratrice Am Pone",
+        "80cm x 60cm",
+        "Mars 2018",
+        ["building"],
+        "94.jpg",
+        false
+    ],
+
+    [
+        "Hermann",
+        "La photo d'une tortue, plein d'effet au niveau du toucher: la peau, la carapace...",
+        "70cm x 50cm",
+        "Février 2018",
+        ["Nature"],
+        "93.jpg",
+        false
+    ],
+
+    [
+        "Les totems des sens (6ème sens)",
+        "L'odorat, la vue, le goût, l'ouîe, le toucher, et... un 6ème sens ?",
+        "30cm x 60cm chaque",
+        "Février 2018",
+        ["Nature"],
+        "92.jpg",
+        false
+    ],
+
+    [
+        "Les totems des sens (toucher)",
+        "L'odorat, la vue, le goût, l'ouîe, le toucher, et... un 6ème sens ?",
+        "30cm x 60cm chaque",
+        "Février 2018",
+        ["Nature"],
+        "91.jpg",
+        false
+    ],
+
+    [
+        "Les totems des sens (ouïe)",
+        "L'odorat, la vue, le goût, l'ouîe, le toucher, et... un 6ème sens ?",
+        "30cm x 60cm chaque",
+        "Février 2018",
+        ["Nature"],
+        "90.jpg",
+        false
+    ],
+
+    [
+        "Les totems des sens (vue)",
+        "L'odorat, la vue, le goût, l'ouîe, le toucher, et... un 6ème sens ?",
+        "30cm x 60cm chaque",
+        "Février 2018",
+        ["Nature"],
+        "89.jpg",
+        false
+    ],
+
+    [
+        "Les totems des sens (goût)",
+        "L'odorat, la vue, le goût, l'ouîe, le toucher, et... un 6ème sens ?",
+        "30cm x 60cm chaque",
+        "Février 2018",
+        ["Nature"],
+        "88.jpg",
+        false
+    ],
+
+    [
+        "Les totems des sens (odorat)",
+        "L'odorat, la vue, le goût, l'ouîe, le toucher, et... un 6ème sens ?",
+        "30cm x 60cm chaque",
+        "Février 2018",
+        ["Nature"],
+        "87.jpg",
+        false
+    ],
+
+    [
+        "Comment un chat passe par là, montre patte blanche et finit par égayer ton quotidien",
+        "L'histoire de chat de mon frère",
+        "80cm x 40cm",
+        "Décembre 2016",
+        ["Chat"],
+        "86.jpg",
+        false
+    ],
+
+    [
+        "Le grand menhir bleu",
+        "Essai technique sur papier photo",
+        "30cm x 21cm",
+        "Mai 2016",
+        ["Paysage","Essais technique"],
+        "85.jpg",
+        false
+    ],
+
+    [
+        "Drôle de maison",
+        "Une maison imaginaire",
+        "36cm x 23cm",
+        "Avril 2016",
+        ["Paysage"],
+        "84.jpg",
+        true
+    ],
+
+    [
+        "Mosa",
+        "La mosaïque",
+        "60cm x 20cm",
+        "Avril 2016",
+        ["Abstrait"],
+        "83.jpg",
+        false
+    ],
+
+    [
+        "Pariétal",
+        "Essai technique sur enduit de rebouchage",
+        "40cm x 50cm",
+        "Mars 2016",
+        ["Abstrait","Essais technique"],
+        "82.jpg",
+        true
+    ],
+
+    [
+        "Les pêcheurs",
+        "Une vieille photo de la Turballe présentée à la Gacilly",
+        "56cm x 47cm",
+        "Mars 2016",
+        ["Paysage", "poisson"],
+        "81.jpg",
+        true
+    ],
+
+    [
+        "Merci M.Manoli",
+        "La visite du sculpteur Pierre Manoli à la Richardais",
+        "30cm x 25cm",
+        "Février 2016",
+        ["Nature","chouette"],
+        "80.jpg",
+        false
+    ],
+
+    [
+        "Les poissons du désert australien",
+        "Les poissons d'un mobile offert à mon fils",
+        "90cm x 50cm",
+        "Janvier 2016",
+        ["Nature", "poisson"],
+        "79-blanco.jpg",
+        false
+    ],
+
+    [
+        "Ph",
+        "Essai technique avec du papier photo",
+        "40cm x 40cm",
+        "Octobre 2015",
+        ["abstrait","Essais technique"],
+        "78.jpg",
+        false
+    ],
+
+    [
+        "L'appât",
+        "Un bout de colle néoprène durci (l'appât)",
+        "70cm x 50cm",
+        "Octobre 2015",
+        ["poisson", "nature"],
+        "77.jpg",
+        false
+    ],
+
+    [
+        "L'arbre des cabanes",
+        "Plume et encre de Chine",
+        "31cm x 26cm",
+        "Septembre 2015",
+        ["paysage", "nature"],
+        "76.jpg",
+        false
+    ],
+
+    [
+        "St Malo",
+        "Une promenade sur la jetée de St Malo",
+        "30cm x 15cm",
+        "Avril 2015",
+        ["paysage"],
+        "75.jpg",
+        false
+    ],
+
+    [
+        "Triptyque poissons",
+        "Couleurs et formes geométriques sur le thème des poissons",
+        "40cm x 40cm chaque",
+        "Avril 2014",
+        ["poisson"],
+        "74-73-72-3.jpg",
+        false
+    ],
+
+    [
+        "Eff",
+        "Essai technique",
+        "40cm x 30cm",
+        "Novembre 2014",
+        ["poisson", "abstrait","Essais technique"],
+        "71.jpg",
+        false
+    ],
+
+    [
+        "Dans la lune",
+        "Des ombres de chouettes sur un fond rouge",
+        "70cm x 40cm",
+        "Novembre 2014",
+        ["nature","chouette"],
+        "70.jpg",
+        false
+    ],
+
+    [
+        "Chat inca",
+        "Recherche de matière",
+        "40cm x 40cm",
+        "Septembre 2014",
+        ["chat", "abstrait"],
+        "69.jpg",
+        false
+    ],
+
+    [
+        "Poissons voyageurs",
+        "Des papiers journaux arabes et coréens, et les poissons",
+        "40cm x 40cm",
+        "Août 2014",
+        ["poisson", "abstrait"],
+        "68-blanco.jpg",
+        false
+    ],
+
+    [
+        "Chouette",
+        "Des rondelles et un réveil cassé",
+        "15cm x 15cm",
+        "Juin 2014",
+        ["abstrait","chouette"],
+        "67.jpg",
+        false
+    ],
+
+    [
+        "Les sentinelles II",
+        "Essais technique: peinture étirée",
+        "70cm x 50cm",
+        "Janvier 2014",
+        ["abstrait","building","essais technique"],
+        "66.jpg",
+        false
+    ],
+
+    [
+        "Poissons bleues",
+        "Répétition d'un motif",
+        "70cm x 50cm",
+        "Novembre 2013",
+        ["poisson"],
+        "65-blanco.jpg",
+        false
+    ],
+];
