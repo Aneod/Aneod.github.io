668b0358e516aec862d5ad27434ec500c8157d36
Blys Digital <135101184+Aneod@users.noreply.github.com>
2023-07-11 19:17:54 +0200

    Add files via upload

---
+++ Blys Boutique/mail.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $retour = mail('blysboutique@gmail.com', 'Envoi depuis la page Contact', $_GET['message'], 'From:blysboutique@gmail.com');
+    if ($retour){
+        echo '<p>Votre message a bien été envoyé.</p>';
+    }
+    ?>
+</body>
+</html>
